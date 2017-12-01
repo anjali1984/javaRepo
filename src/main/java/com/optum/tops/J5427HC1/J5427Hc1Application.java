78e78246a558f77422f32cb911cd9d95cdb84887
Mark Pantojan <mark_pantojan@optum.com>
2017-12-01 15:22:54 -0500

    inital push

---
+++ src/main/java/com/optum/tops/J5427HC1/J5427Hc1Application.java
@@ -0,0 +1,12 @@
+package com.optum.tops.J5427HC1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class J5427Hc1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(J5427Hc1Application.class, args);
+	}
+}
