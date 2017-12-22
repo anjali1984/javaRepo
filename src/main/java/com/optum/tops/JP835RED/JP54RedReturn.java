3dcc09aa2aa622b4ebe46b376dc5d55d4d7dafe4
Mark Pantojan <mark_pantojan@optum.com>
2017-12-22 14:36:57 -0500

    Added JP54RedReturn classes

---
+++ src/main/java/com/optum/tops/JP835RED/JP54RedReturn.java
@@ -0,0 +1,22 @@
+package com.optum.tops.JP835RED;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class JP54RedReturn {
+	
+//	10 :RET:-835-REDUCT-AREA          OCCURS 150 TIMES.
+//    15 :RET:-835-RD-SVC-ID           PIC S9(03) COMP-3.
+//    15 :RET:-835-RD-REV-ID           PIC S9(03) COMP-3.
+//    15 :RET:-835-RD-REV-CD           PIC S9(05) COMP-3.
+//    15 :RET:-835-RD-PROC-CD          PIC X(05).
+//    15 :RET:-835-RD-PROC-TYP-CD      PIC X(01).
+//    15 :RET:-835-RD-CATGY-ID         PIC S9(4) COMP.
+//    15 :RET:-835-RD-GRP-CD           PIC X(02).
+//    15 :RET:-835-RD-CARC-CD          PIC X(05).
+//    15 :RET:-835-RD-RARC-CD          PIC X(06).
+//    15 :RET:-835-RD-PD-AMT           PIC S9(09)V99 COMP-3.
+    
+	private List<Ret835ReductArea> red835ReductArea = new ArrayList<Ret835ReductArea>(150);
+
+}
---
+++ src/main/java/com/optum/tops/JP835RED/Ret835ReductArea.java
@@ -0,0 +1,81 @@
+package com.optum.tops.JP835RED;
+
+import java.math.BigDecimal;
+
+public class Ret835ReductArea {
+
+	private int RET_835_RD_SVC_ID;
+	private int RET_835_RD_REV_ID;
+	private int RET_835_RD_REV_CD;
+	private String RET_835_RD_PROC_CD;
+	private String RET_835_RD_PROC_TYP_CD;
+	private int RET_835_RD_CATGY_ID;
+	private String RET_835_RD_GRP_ID;
+	private String RET_835_RD_CARC_CD;
+	private String RET_835_RD_RARC_CD;
+	private BigDecimal RET_835_RD_PD_AMT;
+	
+	public int getRET_835_RD_SVC_ID() {
+		return RET_835_RD_SVC_ID;
+	}
+	public void setRET_835_RD_SVC_ID(int rET_835_RD_SVC_ID) {
+		RET_835_RD_SVC_ID = rET_835_RD_SVC_ID;
+	}
+	public int getRET_835_RD_REV_ID() {
+		return RET_835_RD_REV_ID;
+	}
+	public void setRET_835_RD_REV_ID(int rET_835_RD_REV_ID) {
+		RET_835_RD_REV_ID = rET_835_RD_REV_ID;
+	}
+	public int getRET_835_RD_REV_CD() {
+		return RET_835_RD_REV_CD;
+	}
+	public void setRET_835_RD_REV_CD(int rET_835_RD_REV_CD) {
+		RET_835_RD_REV_CD = rET_835_RD_REV_CD;
+	}
+	public String getRET_835_RD_PROC_CD() {
+		return RET_835_RD_PROC_CD;
+	}
+	public void setRET_835_RD_PROC_CD(String rET_835_RD_PROC_CD) {
+		RET_835_RD_PROC_CD = rET_835_RD_PROC_CD;
+	}
+	public String getRET_835_RD_PROC_TYP_CD() {
+		return RET_835_RD_PROC_TYP_CD;
+	}
+	public void setRET_835_RD_PROC_TYP_CD(String rET_835_RD_PROC_TYP_CD) {
+		RET_835_RD_PROC_TYP_CD = rET_835_RD_PROC_TYP_CD;
+	}
+	public int getRET_835_RD_CATGY_ID() {
+		return RET_835_RD_CATGY_ID;
+	}
+	public void setRET_835_RD_CATGY_ID(int rET_835_RD_CATGY_ID) {
+		RET_835_RD_CATGY_ID = rET_835_RD_CATGY_ID;
+	}
+	public String getRET_835_RD_GRP_ID() {
+		return RET_835_RD_GRP_ID;
+	}
+	public void setRET_835_RD_GRP_ID(String rET_835_RD_GRP_ID) {
+		RET_835_RD_GRP_ID = rET_835_RD_GRP_ID;
+	}
+	public String getRET_835_RD_CARC_CD() {
+		return RET_835_RD_CARC_CD;
+	}
+	public void setRET_835_RD_CARC_CD(String rET_835_RD_CARC_CD) {
+		RET_835_RD_CARC_CD = rET_835_RD_CARC_CD;
+	}
+	public String getRET_835_RD_RARC_CD() {
+		return RET_835_RD_RARC_CD;
+	}
+	public void setRET_835_RD_RARC_CD(String rET_835_RD_RARC_CD) {
+		RET_835_RD_RARC_CD = rET_835_RD_RARC_CD;
+	}
+	public BigDecimal getRET_835_RD_PD_AMT() {
+		return RET_835_RD_PD_AMT;
+	}
+	public void setRET_835_RD_PD_AMT(BigDecimal rET_835_RD_PD_AMT) {
+		RET_835_RD_PD_AMT = rET_835_RD_PD_AMT;
+	}
+
+	
+	
+}
