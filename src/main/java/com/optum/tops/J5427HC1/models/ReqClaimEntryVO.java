51570a6d9d5e36f3d4845203e920b08c98c62854
Yadav, Anjali <anjlai.yadav@optum.com>
2018-02-08 16:27:35 -0500

    Add files via upload

---
+++ src/main/java/com/optum/tops/J5427HC1/models/ReqClaimEntryVO.java
@@ -0,0 +1,25 @@
+package com.optum.tops.J5427HC1.models;
+
+public class ReqClaimEntryVO {
+	
+	private ReqClaimEntry reqClaimEntry;
+	
+	private String logId;
+
+	public ReqClaimEntry getReqClaimEntry() {
+		return reqClaimEntry;
+	}
+
+	public void setReqClaimEntry(ReqClaimEntry reqClaimEntry) {
+		this.reqClaimEntry = reqClaimEntry;
+	}
+
+	public String getLogId() {
+		return logId;
+	}
+
+	public void setLogId(String logId) {
+		this.logId = logId;
+	}
+	
+}
